2b420cf25a5e1a7fd04327e5da8f846e410f69a4
egwilborn <egwilborn@gmail.com>
2024-02-08 20:36:29 -0600

    adds order function

--- typescript-problems.ts
+++ typescript-problems.ts
@@ -126,6 +126,34 @@ function spinWords(words: string): string {
   return results.join(" ");
 }
 
-spinWords("Hey fellow warriors"); //  --> "Hey wollef sroirraw"
-spinWords("This is a test"); //        --> "This is a test"
-spinWords("This is another test"); // --> "This is rehtona test"
+// spinWords("Hey fellow warriors"); //  --> "Hey wollef sroirraw"
+// spinWords("This is a test"); //        --> "This is a test"
+// spinWords("This is another test"); // --> "This is rehtona test"
+
+// Your task is to sort a given string. Each word in the string will contain a single number.
+// This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input
+// String will only contain valid consecutive numbers.
+
+function order(words: string): string {
+  if (words.length === 0) return "";
+  const wordsArray = words.split(" ");
+  const resultsArray: string[] = [];
+  // loop over array
+  wordsArray.forEach((word: string) => {
+    const regex = /(\d+)/;
+    const index: any[] | null = word?.match(regex);
+    // reassign the word in new array based on number within
+    resultsArray[parseInt(index![0]) - 1] = word;
+  });
+  // join array and return string
+  return resultsArray.join(" ");
+}
+
+// Examples
+order("is2 Thi1s T4est 3a"); //-->  "Thi1s is2 3a T4est"
+order("4of Fo1r pe6ople g3ood th5e the2"); //  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+order(""); //  -->  ""
